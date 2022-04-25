dc55a4b2b56d8a1f851a9380714d808c8c87535a
KaylaStiver <KaylaStiver3@gmail.com>
2022-04-25 11:47:35 -0400

    Add project files.

---
+++ Unit 8 Assignment 1/Vehicle.h
@@ -0,0 +1,22 @@
+// Kayla Stiver
+// CIS 1202 800
+// April 25th 2022
+
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+private:
+	string manufacturer;
+	int year;
+public:
+	Vehicle();
+	void setManu(string);
+	void setYear(int);
+	string setManu;
+	int setYear;
+};
+
